d02da4d5328625e2b197f1c364e5c6d2097d880a
eyasuh <eyha12@gmail.com>
2022-03-05 12:52:40 -0500

    added StudentConfig

---
+++ src/main/java/com/eyasudev/springproject/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.eyasudev.springproject.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository repository) {
+		return args -> {
+			Student chala = new Student(
+					"Chala",
+					"chala@gmail.com",
+					LocalDate.of(1994, Month.APRIL, 20),
+					28
+			);
+
+			Student abebe = new Student(
+					"Abebe",
+					"abebe@gmail.com",
+					LocalDate.of(2000, Month.APRIL, 20),
+					28
+			);
+			repository.saveAll(
+					List.of(chala, abebe)
+			);
+		};
+	}
+}
--- src/main/java/com/eyasudev/springproject/student/StudentService.java
+++ src/main/java/com/eyasudev/springproject/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.eyasudev.springproject.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,23 @@ import java.util.List;
 @Service
 public class StudentService {
 
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
 	public List<Student> getStudents() {
-		return List.of(
-				new Student(
-						1L,
-						"Chala",
-						"chala@gmail.com",
-						LocalDate.of(1994, Month.APRIL, 20),
-						28
-				)
-		);
+		return studentRepository.findAll();
+//		return List.of(
+//				new Student(
+//						1L,
+//						"Chala",
+//						"chala@gmail.com",
+//						LocalDate.of(1994, Month.APRIL, 20),
+//						28
+//				)
+//		);
 	}
 }
